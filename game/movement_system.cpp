70b7a8654a657093b3ac04ee1f88168228c8da5b
mbellman <mbellman@nwe.com>
2022-12-04 16:44:17 -0600

    factoring collision handling

--- game/movement_system.cpp
+++ game/movement_system.cpp
@@ -3,6 +3,23 @@
 
 using namespace Gamma;
 
+internal void handleCollisions(GmContext* context, GameState& state) {
+  auto& player = getPlayer();
+
+  // @todo handle collisions more robustly
+  if (player.position.y < 20.f) {
+    float delta = state.lastFrameY - player.position.y;
+
+    if (state.lastFrameY > 20.f && delta > 2.f) {
+      player.position.y = 20.f;
+      state.velocity.y *= -0.2f;
+    } else {
+      player.position.y = 20.f;
+      state.velocity.y = 0.f;
+    }
+  }
+}
+
 namespace MovementSystem {
   void handlePlayerMovement(GmContext* context, GameState& state, float dt) {
     auto& player = getPlayer();
@@ -12,18 +29,7 @@ namespace MovementSystem {
     state.velocity.y -= gravity;
     player.position += state.velocity * dt;
 
-    // @todo resolve collisions more robustly
-    if (player.position.y < 20.f) {
-      float delta = state.lastFrameY - player.position.y;
-
-      if (state.lastFrameY > 20.f && delta > 2.f) {
-        player.position.y = 20.f;
-        state.velocity.y *= -0.2f;
-      } else {
-        player.position.y = 20.f;
-        state.velocity.y = 0.f;
-      }
-    }
+    handleCollisions(context, state);
 
     // @todo dampen velocity more robustly
     if (player.position.y == 20.f) {
