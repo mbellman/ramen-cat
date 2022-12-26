6ff89c7f063ce6a350fad4084c0f4b89bf2661c7
mbellman <mbellman@nwe.com>
2022-12-25 20:00:00 -0600

    removing errant macro backslashes

--- game/macros.h
+++ game/macros.h
@@ -5,9 +5,9 @@
 
 #define START_TIMING(label) \
   u64 __start = Gm_GetMicroseconds();\
-  std::string __label = label;\
+  std::string __label = label;
 
 #define LOG_TIME() \
     u64 __time = Gm_GetMicroseconds() - __start;\
     u32 __ms = __time / 1000.f;\
-    addDebugMessage(__label + ": " + std::to_string(__time) + " us");\
+    addDebugMessage(__label + ": " + std::to_string(__time) + " us");
