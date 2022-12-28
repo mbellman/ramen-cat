ca71acd67beade37792152662311b7352277d668
mbellman <mbellman@nwe.com>
2022-12-28 01:49:29 -0600

    testing out rect rendering

--- game/ui_system.cpp
+++ game/ui_system.cpp
@@ -1,4 +1,6 @@
+// @todo add first-class engine features for images/shapes/text
 #include "SDL_image.h"
+#include "SDL_ttf.h"
 
 #include "ui_system.h"
 
@@ -6,16 +8,22 @@ using namespace Gamma;
 
 // @temporary
 static SDL_Surface* cat = nullptr;
+static SDL_Surface* textBox = nullptr;
+static TTF_Font* font = nullptr;
 
 void UISystem::initializeUI(GmContext* context, GameState& state) {
   // @temporary
   cat = IMG_Load("./game/ui/cat.png");
+  textBox = SDL_CreateRGBSurface(0, 500, 150, 24, 0, 0, 0, 0);
+  font = TTF_OpenFont("./fonts/OpenSans-Regular.ttf", 32);
+  SDL_FillRect(textBox, 0, 0xFF00FF);
 
-  // @todo load textures
+  // @todo load UI textures
 }
 
 void UISystem::handleUI(GmContext* context, GameState& state, float dt) {
-  auto& renderer = *context->renderer;
-
+  // @temporary
   render_image(cat, 500, 100, 150, 150);
+  render_image(textBox, 300, 400, textBox->w, textBox->h);
+  render_text(font, "Hello there!", 200, 300);
 }
