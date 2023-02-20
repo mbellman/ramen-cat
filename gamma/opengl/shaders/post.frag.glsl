768c12aef9c278b7f64d5266577c32c086f24b65
mbellman <mbellman@nwe.com>
2023-02-20 16:50:48 -0600

    refactoring post shader for readability

--- gamma/opengl/shaders/post.frag.glsl
+++ gamma/opengl/shaders/post.frag.glsl
@@ -22,51 +22,35 @@ layout (location = 0) out vec3 out_color;
 #include "utils/conversion.glsl";
 #include "utils/helpers.glsl";
 
-void main() {
-  // @todo cleanup
-  float aspect_ratio = 1920.0 / 1080.0;
-  vec2 frag_uv_ratio = vec2(1.0, 1 / aspect_ratio);
-  float radius = length(fragUv * frag_uv_ratio - vec2(0.5, 0.5) * frag_uv_ratio);
-  float offset = min(1.0, distance(radius, screenWarpTime));
-  float displacement = pow(1.0 - offset, 30);
-  vec2 adjusted_uv = (fragUv - 0.5) * (1.0 - displacement * 0.1) + 0.5;
-  vec4 base_frag_color_and_depth = texture(texColorAndDepth, fragUv);
-  vec4 warped_frag_color_and_depth = texture(texColorAndDepth, adjusted_uv);
+vec3 getDepthOfFieldColor(vec3 current_out_color, vec2 uv, float linear_frag_depth) {
+  const int MIP_LEVEL = 1;
+  const float MAX_DEPTH = 3000.0;
 
-  #if USE_DEPTH_OF_FIELD == 1
-    {
-      const int MIP_LEVEL = 1;
-      const float MAX_DEPTH = 3000.0;
+  vec2 texel_size = 1.0 / textureSize(texColorAndDepth, MIP_LEVEL);
+  vec3 depth_of_field_color = vec3(0.0);
 
-      vec2 texel_size = 1.0 / textureSize(texColorAndDepth, MIP_LEVEL);
-      vec3 depth_of_field_color = vec3(0.0);
+  vec2 uv1 = uv + vec2(-1.0, 0.0) * texel_size;
+  vec2 uv2 = uv + vec2(1.0, 0.0) * texel_size;
+  vec2 uv3 = uv + vec2(0.0, -1.0) * texel_size;
+  vec2 uv4 = uv + vec2(0.0, 1.0) * texel_size;
 
-      vec2 uv1 = adjusted_uv + vec2(-1.0, 0.0) * texel_size;
-      vec2 uv2 = adjusted_uv + vec2(1.0, 0.0) * texel_size;
-      vec2 uv3 = adjusted_uv + vec2(0.0, -1.0) * texel_size;
-      vec2 uv4 = adjusted_uv + vec2(0.0, 1.0) * texel_size;
+  depth_of_field_color += textureLod(texColorAndDepth, uv, MIP_LEVEL).rgb;
+  depth_of_field_color += textureLod(texColorAndDepth, uv1, MIP_LEVEL).rgb;
+  depth_of_field_color += textureLod(texColorAndDepth, uv2, MIP_LEVEL).rgb;
+  depth_of_field_color += textureLod(texColorAndDepth, uv3, MIP_LEVEL).rgb;
+  depth_of_field_color += textureLod(texColorAndDepth, uv4, MIP_LEVEL).rgb;
+  depth_of_field_color /= 5.0;
 
-      depth_of_field_color += textureLod(texColorAndDepth, adjusted_uv, MIP_LEVEL).rgb;
-      depth_of_field_color += textureLod(texColorAndDepth, uv1, MIP_LEVEL).rgb;
-      depth_of_field_color += textureLod(texColorAndDepth, uv2, MIP_LEVEL).rgb;
-      depth_of_field_color += textureLod(texColorAndDepth, uv3, MIP_LEVEL).rgb;
-      depth_of_field_color += textureLod(texColorAndDepth, uv4, MIP_LEVEL).rgb;
-      depth_of_field_color /= 5.0;
+  float depth_factor = linear_frag_depth / MAX_DEPTH;
+  if (depth_factor > 1.0) depth_factor = 1.0;
 
-      float depth_factor = getLinearizedDepth(base_frag_color_and_depth.w, zNear, zFar) / MAX_DEPTH;
+  depth_factor *= depth_factor;
 
-      if (depth_factor > 1.0) depth_factor = 1.0;
+  return mix(current_out_color, depth_of_field_color, depth_factor);
+}
 
-      depth_factor *= depth_factor;
-
-      out_color = mix(warped_frag_color_and_depth.rgb, depth_of_field_color, depth_factor);
-    }
-  #else
-    out_color = warped_frag_color_and_depth.rgb;
-  #endif
-
-  // @todo make atmospherics optional via a flag
-  vec3 sky_position = getWorldPosition(1.0, adjusted_uv, matInverseProjection, matInverseView) - cameraPosition;
+vec3 getAtmosphericsColor(vec3 current_out_color, vec2 uv, float frag_depth, float linear_frag_depth) {
+  vec3 sky_position = getWorldPosition(1.0, uv, matInverseProjection, matInverseView) - cameraPosition;
   vec3 sky_direction = normalize(sky_position);
 
   // @todo make configurable
@@ -75,8 +59,7 @@ void main() {
   vec2 horizon_direction_2d = normalize(vec2(zFar, -altitude_above_horizon));
   vec2 sky_direction_2d = normalize(vec2(length(sky_direction.xz), sky_direction.y));
 
-  float depth_divisor = base_frag_color_and_depth.w == 1.0 ? zFar : zFar * 0.85;
-  float linear_frag_depth = getLinearizedDepth(base_frag_color_and_depth.w, zNear, zFar);
+  float depth_divisor = frag_depth == 1.0 ? zFar : zFar * 0.85;
   float atmosphere_factor = linear_frag_depth / depth_divisor;
 
   atmosphere_factor *= sky_direction_2d.y < horizon_direction_2d.y ? 1.0 : pow(dot(sky_direction_2d, horizon_direction_2d), 100);
@@ -85,15 +68,18 @@ void main() {
 
   vec3 atmosphere_color = mix(atmosphereColor, vec3(1), 0.5);
 
-  out_color = mix(out_color, atmosphere_color, atmosphere_factor);
+  return mix(current_out_color, atmosphere_color, atmosphere_factor);
+}
 
-  // @todo make optional
-  float spread = 1.5;
+vec3 getToonShadedColor(vec3 current_out_color, vec2 uv, float linear_frag_depth) {
+  // @todo make configurable
+  const float OUTLINE_THICKNESS = 1.5;
+
   vec2 texel_size = 1.0 / textureSize(texColorAndDepth, 0);
-  vec2 uv1 = fragUv + texel_size * vec2(-1.0, 0) * spread;
-  vec2 uv2 = fragUv + texel_size * vec2(1.0, 0) * spread;
-  vec2 uv3 = fragUv + texel_size * vec2(0, -1.0) * spread;
-  vec2 uv4 = fragUv + texel_size * vec2(0, 1.0) * spread;
+  vec2 uv1 = uv + texel_size * vec2(-1.0, 0) * OUTLINE_THICKNESS;
+  vec2 uv2 = uv + texel_size * vec2(1.0, 0) * OUTLINE_THICKNESS;
+  vec2 uv3 = uv + texel_size * vec2(0, -1.0) * OUTLINE_THICKNESS;
+  vec2 uv4 = uv + texel_size * vec2(0, 1.0) * OUTLINE_THICKNESS;
   float depth1 = getLinearizedDepth(texture(texColorAndDepth, uv1).w, zNear, zFar);
   float depth2 = getLinearizedDepth(texture(texColorAndDepth, uv2).w, zNear, zFar);
   float depth3 = getLinearizedDepth(texture(texColorAndDepth, uv3).w, zNear, zFar);
@@ -110,8 +96,34 @@ void main() {
   ) {
     float alpha = saturate((linear_frag_depth + zFar * 0.5) / zFar);
 
-    out_color = mix(vec3(0), out_color, alpha);
+    current_out_color = mix(vec3(0), current_out_color, alpha);
   }
+
+  return current_out_color;
+}
+
+void main() {
+  // Apply screen warp
+  float aspect_ratio = 1920.0 / 1080.0;
+  vec2 frag_uv_ratio = vec2(1.0, 1 / aspect_ratio);
+  float screen_warp_radius = length(fragUv * frag_uv_ratio - vec2(0.5, 0.5) * frag_uv_ratio);
+  float screen_warp_offset = min(1.0, distance(screen_warp_radius, screenWarpTime));
+  float screen_warp_displacement = pow(1.0 - screen_warp_offset, 30);
+  vec2 screen_warp_uv = (fragUv - 0.5) * (1.0 - screen_warp_displacement * 0.1) + 0.5;
+  vec4 frag_color_and_depth = texture(texColorAndDepth, screen_warp_uv);
+  float linear_frag_depth = getLinearizedDepth(frag_color_and_depth.w, zNear, zFar);
+
+  out_color = frag_color_and_depth.rgb;
+
+  #if USE_DEPTH_OF_FIELD == 1
+    out_color = getDepthOfFieldColor(out_color, screen_warp_uv, linear_frag_depth);
+  #else
+    out_color = frag_color_and_depth.rgb;
+  #endif
+
+  // @todo make atmospherics optional via a flag
+  out_color = getAtmosphericsColor(out_color, screen_warp_uv, frag_color_and_depth.w, linear_frag_depth);
+  out_color = getToonShadedColor(out_color, screen_warp_uv, linear_frag_depth);
 
   // @todo gamma correction/tone-mapping
   // out_color = pow(out_color, vec3(1 / 2.2));
