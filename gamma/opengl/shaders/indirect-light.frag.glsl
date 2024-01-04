69a72588f8b7f73c35ba8c166ed8ce8844d8a129
mbellman <mbellman@nwe.com>
2024-01-03 22:38:05 -0600

    fixing far-field SSAO artifacts

--- gamma/opengl/shaders/indirect-light.frag.glsl
+++ gamma/opengl/shaders/indirect-light.frag.glsl
@@ -66,8 +66,8 @@ vec2 rotatedVogelDisc(int samples, int index) {
 float getScreenSpaceAmbientOcclusionContribution(float fragment_depth, vec3 fragment_position, vec3 fragment_normal) {
   const int TOTAL_SAMPLES = 16;
   float linearized_fragment_depth = getLinearizedDepth(fragment_depth, zNear, zFar);
-  float radius_depth_ratio = saturate(linearized_fragment_depth / (zFar * 0.1));
-  float radius = mix(16.0, (zFar / 100.0), radius_depth_ratio);
+  float radius_depth_ratio = saturate(linearized_fragment_depth / (zFar * 0.5));
+  float radius = mix(16.0, (zFar / 20.0), radius_depth_ratio);
   float occlusion = 0.0;
 
   #if USE_DENOISING == 1
