9a1deaf10d88ff1f3bb1982379b77e41ba9e9bbe
mbellman <mbellman@nwe.com>
2024-03-03 21:14:10 -0600

    overworld updates

--- game/effects_system.cpp
+++ game/effects_system.cpp
@@ -384,6 +384,7 @@ void EffectsSystem::updateDayNightCycleLighting(GmContext* context, GameState& s
     mesh("solar-turbine")->emissivity = lightSourceEmissivity * 0.2f;
 
     mesh("lantern")->emissivity = lanternEmissivity;
+    mesh("bathhouse-balloon")->emissivity = 0.2f + 0.8f * lanternEmissivity;
     mesh("orange-lantern")->emissivity = 0.4f + 0.3f * lanternEmissivity;
     mesh("streetlamp-light")->emissivity = lanternEmissivity;
 
